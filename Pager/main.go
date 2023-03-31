39246d5ebceeabce80369b8311dbf0d685659ed3
Zach Young <zacharysyoung@gmail.com>
2023-03-31 05:08:59 -0700

    working

--- Pager/main.go
+++ Pager/main.go
@@ -17,29 +17,64 @@ const (
 var (
 	apiKey string
 
-	a  *airtable.Airtable
-	p  airtable.Parameters
-	lo airtable.AirtableList
-	o  string
+	// a  *airtable.Airtable
+	// p  airtable.Parameters
+	// lo airtable.AirtableList
+	// o  string
 )
 
-const initOffset = "InitialOffset"
+const initOffset = "InitialOffset" // Need a non-empty value for first iteration
 
 func init() {
 	b, _ := os.ReadFile(".secret.txt")
 	apiKey = string(b)
 
-	a = airtable.New(apiKey, appID, false)
-	p = airtable.Parameters{
-		Name:       tblID,
-		MaxRecords: "10",
-		PageSize:   "3",
-	}
-	o = initOffset
+	// a = airtable.New(apiKey, appID, false)
+	// p = airtable.Parameters{
+	// 	Name:       tblID,
+	// 	MaxRecords: "10",
+	// 	PageSize:   "3",
+	// }
+	// o = initOffset
+}
+
+type ListPager struct {
+	at *airtable.Airtable
+	pm airtable.Parameters
+	lo airtable.AirtableList
+
+	o string
+}
+
+func NewListPager(at *airtable.Airtable, pm airtable.Parameters, lo airtable.AirtableList) *ListPager {
+	return &ListPager{at, pm, lo, initOffset}
 }
 
 var ErrEOL = errors.New("no more pages in list")
 
+func (lp *ListPager) Next() (records []airtable.AirtableItem, err error) {
+	switch lp.o {
+	case "":
+		return nil, ErrEOL
+	case initOffset:
+		lp.o = ""
+	}
+
+	lp.pm.Offset = lp.o
+	lp.lo.Offset = "" // lo.Offset must be reset everytime
+	if err := lp.at.List(lp.pm, &lp.lo); err != nil {
+		return nil, err
+	}
+	lp.o = lp.lo.Offset
+
+	return lp.lo.Records, nil
+}
+
+func (p *ListPager) Offset() string {
+	return p.o
+}
+
+/*
 func next() error {
 	switch o {
 	case "":
@@ -57,7 +92,36 @@ func next() error {
 
 	return nil
 }
+*/
 
+func main() {
+	var (
+		a = airtable.New(apiKey, appID, false)
+		p = airtable.Parameters{
+			Name: tblID,
+			// MaxRecords: "10",
+			// PageSize:   "3",
+		}
+		l airtable.AirtableList
+	)
+
+	pgr := NewListPager(a, p, l)
+	for {
+		records, err := pgr.Next()
+		if err != nil {
+			if err == ErrEOL {
+				break
+			}
+			log.Fatal(err)
+		}
+		fmt.Println("o:", pgr.Offset())
+		for _, record := range records {
+			fmt.Println("  r:", record.ID)
+		}
+	}
+}
+
+/* using next()
 func main() {
 	for {
 		if err := next(); err != nil {
@@ -71,8 +135,9 @@ func main() {
 }
 
 func printLO() {
-	fmt.Println("offset:", lo.Offset)
+	fmt.Println("o:", lo.Offset)
 	for _, record := range lo.Records {
-		fmt.Println("    recID:", record.ID)
+		fmt.Println("  r:", record.ID)
 	}
 }
+*/
